a49dc0a437a73bcebad872d2ad5d07354ef1a4d1
Sebastian Puente <sebas7pr@gmail.com>
2014-10-05 01:26:21 -0500

    Whitelisting parameters to accept post requests

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -18,7 +18,7 @@ class TechniquesController < ApplicationController
   # POST /techniques
   # POST /techniques.json
   def create
-    @technique = Technique.new(params[:technique])
+    @technique = Technique.new(technique_params)
 
     if @technique.save
       render json: @technique, status: :created, location: @technique
@@ -46,5 +46,12 @@ class TechniquesController < ApplicationController
     @technique.destroy
 
     head :no_content
+  end
+
+  # Whitelisting Action Controller parameters,
+  # so they can be used in Active Model mass assignments.
+  private
+  def technique_params
+    params.require(:technique).permit(:name, :description, :status)
   end
 end
